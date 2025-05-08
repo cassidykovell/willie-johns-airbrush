4787e12e19952a92c37526fe66dccb88de737c2a
Cassidy <cassidy0kovell@gmail.com>
2025-05-08 14:32:11 -0700

    fixed nav issue

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import { Outlet } from 'react-router-dom';
 import Header from './components/Header';
-import Nav from './components/Nav';
+import Nav from './components/NAv';
 import Footer from './components/Footer';
 import './index.css';
 
