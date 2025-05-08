1e19267501816b44af975c7229bf471576187454
Cassidy <cassidy0kovell@gmail.com>
2025-05-08 14:35:44 -0700

    maybe fixed nav

--- src/components/Nav.jsx
+++ src/components/Nav.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 import { Link } from 'react-router-dom';
 import '../assets/css/style.css'
 
-const Nav = () => {
+const NAv = () => {
   return (
     <nav>
       <ul>
@@ -14,4 +14,4 @@ const Nav = () => {
   );
 };
 
-export default Nav;
+export default NAv;
